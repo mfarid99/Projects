705ca18b12616ddf6df616a747c3b129dc741a16
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-08-26 23:39:19 -0400

    further added slideshow

--- p1/Project1Water/app.js
+++ p1/Project1Water/app.js
@@ -3,20 +3,55 @@
 //ae6868eb-a85d-4057-90cb-e2bfa99f07a9
 //https://content.guardianapis.com/?api-key=ae6868eb-a85d-4057-90cb-e2bfa99f07a9
 
+var width = 720;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
 $('form').on('submit', (event)=>{
 
     event.preventDefault();
+    $('.guardiangen').fadeOut(2000)
 
     const news = $('input[type="text"]').val();
+
+   
     
 
 
-$.ajax({
+    $.ajax({
     url: `https://content.guardianapis.com/search?q=${news}&api-key=ae6868eb-a85d-4057-90cb-e2bfa99f07a9`,
 
     
-}).then(
-    (data)=>{
+    }).then(
+        (data)=>{
         console.log(data)
          
         // $('#newsBrief').html(data.response.results[0].webTitle);
@@ -31,7 +66,8 @@ $.ajax({
 
             const $news = $(`<p>Article: ${result.webTitle} </p>`)
             const $url= $(`<p>`)
-            $url.html (`<a href = {result.webUrl}> ${result.webUrl} </a>`)
+            $url.html (`<a href = ${result.webUrl}> ${result.webUrl}  </a>`);
+            // $url.html (`<a href={result.webUrl}>${result.webUrl}</a>`)
             $news.addClass('news')
             $url.addClass('url')
             
@@ -45,12 +81,14 @@ $.ajax({
         // const url = `${result.webUrl}.css('background-color', 'red')
         
     },
-
+    
         
     ()=>{ 
         console.log('bad request');
     }
+    
     );
+    
 })
 
 
